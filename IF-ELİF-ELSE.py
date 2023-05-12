159651f20211ef8529c7e72ba91ffa19b91dec59
Mehmet <132574963+Mhmt-0@users.noreply.github.com>
2023-05-12 13:17:43 +0300

    Update IF-ELİF-ELSE.py

--- IF-ELİF-ELSE.py
+++ IF-ELİF-ELSE.py
@@ -1,4 +1,4 @@
-"""
+
 sayı_1 = int(input("Sayı girin"))
 sayı_2 = int(input("Sayı girin"))
 
@@ -6,7 +6,7 @@ if sayı_1 > sayı_2:
     print(f'{sayı_1} büyüktür')
 else:
     print(f'{sayı_2} büyüktür')
-"""
+
 
 
 number = int(input("Sayı Giriniz"))
@@ -20,7 +20,7 @@ else:
 
 
 
-"""
+
 product = input('Ürün Giriniz ')
 
 if product == 'muz' and product == 'ananas' or product == 'elma':
@@ -31,10 +31,10 @@ elif product == 'şampuan' or product == 'parfüm' or product == 'diş macunu':
     print('Kozmetik Reyonu')
 else:
     print('Aradğınız Ürünü Bulunmamaktadır')
-"""
 
 
-"""
+
+
 ad_soyad = input("Lütfen tam adınızı girin: ")
 mail_adresi = ad_soyad.lower().replace(" ", ".") + "@bilgeadam.com"
 print("mehmet.orman@bilgeadam.com: ", mail_adresi)
@@ -44,4 +44,3 @@ print("mehmet.orman@bilgeadam.com: ", mail_adresi)
 full_name = input('Tam adınız: ').lower()
 name_list = full_name.split(' ')
 print(f'{name_list[0]}.{name_list[-1]}@bilgeadam.com')
-"""
